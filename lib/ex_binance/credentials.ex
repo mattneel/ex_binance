1ed3d8d26a753079f6122db8ffca33ebbcc4ec8d
Matt Neel <matt@codeojo.com>
2021-09-17 13:52:17 -1000

    HACK: Add endpoint to credentials, and override in Spot client when sent.

--- lib/ex_binance/credentials.ex
+++ lib/ex_binance/credentials.ex
@@ -1,9 +1,10 @@
 defmodule ExBinance.Credentials do
   @type t :: %ExBinance.Credentials{
           api_key: String.t(),
-          secret_key: String.t()
+          secret_key: String.t(),
+          endpoint: String.t()
         }
 
   @enforce_keys ~w(api_key secret_key)a
-  defstruct ~w(api_key secret_key)a
+  defstruct ~w(api_key secret_key endpoint)a
 end
--- lib/ex_binance/rest/spot_client.ex
+++ lib/ex_binance/rest/spot_client.ex
@@ -18,23 +18,30 @@ defmodule ExBinance.Rest.SpotClient do
   end
 
   def get(path, params, %Credentials{} = credentials, opts) when is_map(params) do
-    HTTPClient.spot_endpoint() |> HTTPClient.get(path, params, credentials, opts)
+    get_endpoint(credentials) |> HTTPClient.get(path, params, credentials, opts)
   end
 
   @spec post(path, map, credentials, keyword) :: {:ok, any} | {:error, shared_errors}
   def post(path, params, %Credentials{} = credentials, opts \\ [])
       when is_map(params) do
-    HTTPClient.spot_endpoint() |> HTTPClient.post(path, params, credentials, opts)
+        get_endpoint(credentials) |> HTTPClient.post(path, params, credentials, opts)
   end
 
   @spec delete(path, map, credentials, keyword) :: {:ok, any} | {:error, shared_errors}
   def delete(path, params, %Credentials{} = credentials, opts \\ [])
       when is_map(params) do
-    HTTPClient.spot_endpoint() |> HTTPClient.delete(path, params, credentials, opts)
+        get_endpoint(credentials) |> HTTPClient.delete(path, params, credentials, opts)
   end
 
   @spec put(path, map, credentials, keyword) :: {:ok, any} | {:error, shared_errors}
   def put(path, params, %Credentials{} = credentials, opts \\ []) when is_map(params) do
-    HTTPClient.spot_endpoint() |> HTTPClient.put(path, params, credentials, opts)
+    get_endpoint(credentials) |> HTTPClient.put(path, params, credentials, opts)
+  end
+
+  defp get_endpoint(%Credentials{endpoint: endpoint}) do
+    case endpoint do
+      nil -> HTTPClient.spot_endpoint()
+      endpoint -> endpoint
+    end
   end
 end
